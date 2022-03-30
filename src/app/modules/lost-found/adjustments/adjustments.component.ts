d7386674f78c1559d27a24de2ac6ddfc5b3407e3
Sari Marzel <212413751@PC5-6>
2022-03-30 16:45:18 +0300

    aaa

--- src/app/modules/lost-found/adjustments/adjustments.component.ts
+++ src/app/modules/lost-found/adjustments/adjustments.component.ts
@@ -95,7 +95,7 @@ export class AdjustmentsComponent implements OnInit {
           alert("נשלח בהצלחה!")
           //row.incStatusMail()
           //this.ADJUSTMENTS[index]=row
-          this.adjustmentService.getAdjustments(this.LFId)
+          this.adjustmentService.getAdjustments(this.LFId)////hgyuhoui
           //לעדכן רשימה בסרבר ולהביא לקליינט
         }
         else
